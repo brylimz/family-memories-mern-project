c28e93804e72266da1518c736e128e04b9f259cb
brylimz <jackjaggeriv@gmail.com>
2022-04-26 09:21:11 +0700

    createDelete

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -36,9 +36,8 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
 
-
     dispatch({ type: "DELETE", payload: id });
   } catch (error) {
     console.log(error);
   }
-}
+};
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
   switch (action.type) {
+    case 'DELETE':
+    return posts.filter((post) => post._id !== action.payload)
+
     case "UPDATE":
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
@@ -12,5 +15,8 @@ export default (posts = [], action) => {
       return [...posts, action.payload];
     default:
       return posts;
+
+
+
   }
 };
