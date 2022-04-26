a30a888cf94329031109813daf194c5bb2b835fc
brylimz <jackjaggeriv@gmail.com>
2022-04-26 14:28:26 +0700

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          May's Memories
+          Our Memories
         </Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+const url = "https://family-memoriessss.herokuapp.com/posts";
 
 export const fetchPost = () => axios.get(url);
 
