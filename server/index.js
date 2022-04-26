15c3bd7e64926be0ef36feb06f3981ef182e57c3
brylimz <jackjaggeriv@gmail.com>
2022-04-26 10:56:05 +0700

    setup env, fixing bugs

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
 
 const app = express();
-
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "300mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "300mb", extended: true }));
@@ -13,12 +15,12 @@ app.use(bodyParser.urlencoded({ limit: "300mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://javascriptmastery:123@cluster0.ipy8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://javascriptmastery:123@cluster0.ipy8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
